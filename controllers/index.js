f23ff8924722fb1c2bb6f4b65c033ade125840b6
DarrylSimpson <darryl.simpson0793@gmail.com>
2020-12-06 23:23:47 -0600

    create homepage

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
-
 const apiRoutes = require('./api');
+const homeroutes = require('./home-routes.js');
 
 router.use('/api', apiRoutes);
+router.use('/', homeroutes);
 
 router.use((req, res) => {
   res.status(404).end();
